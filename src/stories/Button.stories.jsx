7ea0e9bd84c7af6ea4d72f000f8132fafaaa110f
AmirHossein <Amirho3eint47@gmail.com>
2022-03-09 13:33:00 +0330

    Simplify story

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -5,14 +5,6 @@ import { Button } from "../Button";
 export default {
   title: "Example/Button",
   component: Button,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 };
 
-const Template = (args) => <Button {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  label: "Button",
-};
+export const Button1 = () => <Button label="Button1" />;
